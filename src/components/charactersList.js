86575b73a7f405817af7d10e466d61e9fe8a6247
Marcus Vinícius <marcus.varocha@gmail.com>
2024-01-09 11:08:00 -0300

    .

--- src/components/charactersList.js
+++ src/components/charactersList.js
@@ -11,39 +11,30 @@ function CharactersList({ searchValue, pageValue, pageLimit }) {
   useEffect(() => {
 
     if (searchValue === "") {
-      // Aqui você pode fazer a requisição usando o Axios
       axios.get(`https://rickandmortyapi.com/api/character?page=${pageValue}`)
         .then(response => {
-          console.log(response.data)
-          // Se a requisição for bem-sucedida, atualize o estado com os dados recebidos
           setDados(response.data);
           pageLimit(response.data.info.pages);
         })
         .catch(error => {
-          // Se houver algum erro na requisição, trate o erro aqui
           console.error('Erro ao buscar dados:', error);
         });
     }
     else {
-      // Aqui você pode fazer a requisição usando o Axios
       axios.get(`https://rickandmortyapi.com/api/character?page=${pageValue}&name=${searchValue}`)
         .then(response => {
-          console.log(response.data)
-          // Se a requisição for bem-sucedida, atualize o estado com os dados recebidos
           setDados(response.data);
           pageLimit(response.data.info.pages);
         })
         .catch(error => {
-          // Se houver algum erro na requisição, trate o erro aqui
           console.error('Erro ao buscar dados:', error);
         });
     }
 
-  }, [searchValue, pageValue]); // O array vazio como segundo argumento do useEffect faz com que a requisição seja feita apenas uma vez, ao montar o componente
+  }, [searchValue, pageValue]);
 
   return (
     <div>
-      {/* Aqui você pode usar os dados recebidos na sua interface */}
       {dados ? (
         <div className='characters'>{dados.results.map((character) => (
           <div className='character' key={character.id}>
